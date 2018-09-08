57b950a119dbdafec0ae0a843793a4175c19aac0
hadiburpee <hadiburpee@gmail.com>
2018-09-07 22:54:41 -0700

    added js functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,27 +5,55 @@
 var wordBank = ["space", "earth", "sun", "galaxy", "saturn", "pillars of creation", "constellations"];
 
 
+//creates a random number that is multipled by the length of the wordBank
+var randomNumber = Math.floor(Math.random() * wordBank.length);
+    console.log(randomNumber);
 
-for (i=0; i< wordBank.length; i++){
-    console.log(wordBank[i]);
+//splits the word into letters
 
-}
-
-var splitLetter = wordBank[1].split("");
+var splitLetter = wordBank[randomNumber].split("");
 
     console.log(splitLetter);
 
-// This function is run whenever the user presses a key.
+//Changes the underlines for the letters to the length of the word
+function lineFix(){
+    var line1 = "_ ";
+    var countLine = " ";
+    for(i=0; i<splitLetter.length; i++){
+        countLine = line1 + countLine;
+    }
+    console.log(line1);
+    document.getElementById("demo").innerHTML = countLine;
+}
+
+//Checking what key is pressed.  Will modify to work for the game
 document.onkeyup = function(event) {
-    // Determines which key was pressed.
+
     var userGuess = event.key;
     console.log("A key was pressed" + " - " + userGuess + " - ");
 
-if (userGuess === splitLetter[0]){
+    for(i=0; i<splitLetter.length; i++){
+        if (userGuess === splitLetter[i]){
+            console.log("you guessed right");
+        }
+        else if (userGuess != splitLetter[i]){
+            console.log("you guessed wrong");
 
-    console.log("you guessed right");
+        }
+
 
 }
+}
+//Testing get element by Id function
+function myFunction(){
+    document.getElementById("demo").innerHTML = "Changed";
+
+
+
+}
+
+function my2ndFunction(e, a, r, t, h){
+    console.log(e, a, r, t, h);
 
 }
 
