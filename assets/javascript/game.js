6724462c5f241ce42f68e4f138ceb659825b996f
hadiburpee <hadiburpee@gmail.com>
2018-09-09 11:21:43 -0700

    partial game complete, letters to screen and fill _'s

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,14 +3,14 @@
 
 //declare and initialize wordBank array
 var wordBank = ["space", "earth", "sun", "galaxy", "saturn", "pillars of creation", "constellations"];
-
+var wordSplit = [];
+var countLine = [];
 
 //creates a random number that is multipled by the length of the wordBank
 var randomNumber = Math.floor(Math.random() * wordBank.length);
     console.log(randomNumber);
 
 //splits the word into letters
-
 var splitLetter = wordBank[randomNumber].split("");
 
     console.log(splitLetter);
@@ -18,17 +18,21 @@ var splitLetter = wordBank[randomNumber].split("");
 //Changes the underlines for the letters to the length of the word
 function lineFix(){
     var line1 = "_ ";
-    var countLine = " ";
-    var line2 = [];
+    // countLine = " ";
     
     //trying to add an array for the _ so that each _ has a location which can then be
     //filled with the corresponding letter.  In a for loop, they should have the same index.
     for(i=0; i<splitLetter.length; i++){
-        countLine = line1 + countLine;
-        // line2[i]= " _ ";
+        // countLine = line1 + countLine;
+        countLine[i] = line1;
+        wordSplit[i]=splitLetter[i];
+        console.log(wordSplit);
     }
     console.log(countLine);
-    document.getElementById("demo").innerHTML = countLine;
+    console.log(wordSplit);
+    var x = document.getElementById("demo");
+    x.innerHTML = countLine.join(" ");
+    
 }
 
 //Checking what key is pressed.  Will modify to work for the game
@@ -38,15 +42,20 @@ document.onkeyup = function(event) {
     console.log("A key was pressed" + " - " + userGuess + " - ");
 
     for(i=0; i<splitLetter.length; i++){
+
         if (userGuess === splitLetter[i]){
-            console.log("you guessed right");
+
+            countLine[i] = userGuess;
+            console.log(countLine[i]);
+            console.log(splitLetter[i]);
         }
-        else if (userGuess != splitLetter[i]){
-            console.log("you guessed wrong");
+        // else if (userGuess != splitLetter[i]){
+        //     console.log("you guessed wrong");
 
-        }
+        // }
 
-
+        var x = document.getElementById("demo");
+        x.innerHTML = countLine.join(" ");
 }
 }
 //Testing get element by Id function
@@ -57,10 +66,7 @@ function myFunction(){
 
 }
 
-function my2ndFunction(e, a, r, t, h){
-    console.log(e, a, r, t, h);
 
-}
 
 
 
